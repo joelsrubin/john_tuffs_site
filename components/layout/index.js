29c3a431b2afd162b5cbcc1cf160bcae99c30fcb
joelsrubin <joelsrubin@gmail.com>
2021-04-23 11:45:36 -0500

    styling solidified - mobile/desktop contact page finished / ish

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from './header.module.css';
+
 import { useState, useEffect } from 'react';
 
 
@@ -8,18 +9,30 @@ export default function Layout({ children }) {
 
   const [page, setPage] = useState('Home');
   const [clicked, setClicked] = useState(false);
+  const [width, setWidth] = useState(700);
   useEffect(() => {
     setPage(children.type.name);
-    setClicked(false);
+    setClicked(!clicked);
   }, [children]);
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
 
-  console.log('page is: ', page);
+  let isMobile = (width <= 600);
+
+
   return (
     <>
       <Head>
         <title>
           NEW YORK PRENUPS
-      </title>
+        </title>
       </Head>
       <div>
         <header className={styles.container}>
@@ -31,7 +44,33 @@ export default function Layout({ children }) {
             <div className={styles.slice}></div>
             <div className={styles.slice}></div>
           </div>
-          <ul className={clicked ? styles.clickedList : styles.list}>
+          <ul className={!isMobile ? styles.list : styles.hidden}>
+
+            <Link href="/">
+              <li className={page === 'Home' ? styles.clicked : styles.unclicked}>Home</li>
+            </Link>
+            <Link href="about">
+              <li className={page === 'About' ? styles.clicked : styles.unclicked}>About John</li>
+            </Link>
+            <Link href="FAQs">
+              <li className={page === 'FAQ' ? styles.clicked : styles.unclicked}>Why Get a Prenup?</li>
+            </Link>
+            <Link href="contact">
+              <li className={page === 'Contact' ? styles.clicked : styles.unclicked}>Contact</li>
+            </Link>
+          </ul>
+          <ul className={isMobile ? styles.mobileList : styles.hidden} style={clicked ? { transform: "translateX(-100%)", padding: "40px" } : { transform: "translateX(0)" }}>
+            <h1 onClick={() => setClicked(!clicked)} style={isMobile ? {
+              display: "block",
+              cursor: "pointer",
+              fontSize: "16px",
+              position: "absolute",
+              top: "0",
+              right: "25px",
+              fontSize: "36px",
+              marginLeft: "50px",
+              fontWeight: "400"
+            } : { display: "none" }}>x</h1>
             <Link href="/">
               <li className={page === 'Home' ? styles.clicked : styles.unclicked}>Home</li>
             </Link>
@@ -52,4 +91,7 @@ export default function Layout({ children }) {
       </div>
     </>
   );
+
+
+
 }
--- pages/components/Form.js
+++ pages/components/Form.js
@@ -1,18 +1,20 @@
 import styles from '../../styles/contact.module.css';
+import { useForm } from 'react-hook-form';
 
 export default function Form({ setName, setEmail, setMessage, handleSubmit }) {
-
+  const { register, formState: { errors } } = useForm();
   return (
     <form className={styles.form} id='form'>
-
-      <input type="text" name="name" onChange={(e) => setName(e.target.value)} required />
       <span className={styles.label}>Name</span>
+      <input {...register("name", { required: true })} type="text" name="name" onChange={(e) => setName(e.target.value)} required />
+      {errors.firstName && "First name is required"}
 
-      <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} required />
+
       <span className={styles.label}>Email</span>
+      <input type="email" name="email" onChange={(e) => setEmail(e.target.value)} required />
 
-      <textarea rows="10" name="message" onChange={(e) => setMessage(e.target.value)} required />
       <span className={styles.label}>How can we help?</span>
+      <textarea rows="10" name="message" onChange={(e) => setMessage(e.target.value)} required />
       <button type="submit" className={styles.submit} onClick={handleSubmit}>Send</button>
     </form>
   );
